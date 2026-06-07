2f6abb0ad8d1218c87a8be966f498fac02cc26f4
kirigaya7447 <joaoh2915@gmail.com>
2026-06-06 21:02:42 -0300

    Criando menu base

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,55 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int opcao = 0;
+        Scanner leia = new Scanner(System.in);
+
+        System.out.println("\\\\\\\\\\Seja bem vindo ao sistema de gestão da/////");
+        System.out.println("\\\\\\\\\\AUTO CENTER ROTA 381/////");
+        do {
+            System.out.println("Selecione a opção do menu desejada:");
+            System.out.println("1 - Cadastrar mecânico");
+            System.out.println("2 - Cadastrar cliente");
+            System.out.println("3 - Cadastrar nova peça");
+            System.out.println("4 - Cadastrar Ordem de Serviço");
+            System.out.println("5 - Listar Estoque");
+            System.out.println("6 - Comissão da Equipe");
+            System.out.println("7 - Inventário Crítico");
+            System.out.println("8 - Faturamento de Peças");
+            System.out.println("0 - Sair");
+            System.out.print("Opção desejada: ");
+            
+            opcao = leia.nextInt();
+
+            switch(opcao){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 0:
+                    System.out.println("Obrigado por utilizar nosso sistema!");
+                    System.out.println("Salvando dados...");
+                    System.out.println("Encerrando...");
+                    break;
+                default:
+                    System.out.println("Opção inválida, por favor escolha uma das opções abaixo:");
+                    opcao = -1;
+                    break;
+            }
+        } while (opcao != 0);
+
     }
 }
