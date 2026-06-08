1cd94f679b7e944c54fc8aeb6da20be9a5927a71
guimaraesgiulia <giuliaguimaraesandrade@gmail.com>
2026-06-07 23:36:21 -0300

    Adição do cadastro de mecânicos e peças (estoque)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
 import java.util.Scanner;
 
+import Cadastros.Estoque;
+import Cadastros.Mecanicos;
+import Cadastros.OrdemServico;
+import Cadastros.Veiculos;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         int opcao = 0;
         Scanner leia = new Scanner(System.in);
+
+        Mecanicos[] mecanicos = new Mecanicos[100];
+        Veiculos[] veiculos = new Veiculos[100];
+        Estoque[] estoque = new Estoque[100];
+        OrdemServico[] os = new OrdemServico[100];
+
+        int qtdMecanicos = 0;
+        int qtdVeiculos = 0;
+        int qtdEstoque = 0;
+        int qtdOS = 0;
 
         System.out.println("\\\\\\\\\\Seja bem vindo ao sistema de gestão da/////");
         System.out.println("\\\\\\\\\\AUTO CENTER ROTA 381/////");
@@ -24,10 +39,48 @@ public class Main {
 
             switch(opcao){
                 case 1:
+                    mecanicos[qtdMecanicos] = new Mecanicos();
+
+                    System.out.print("ID: ");
+                    mecanicos[qtdMecanicos].id = leia.nextInt();
+
+                    leia.nextLine();
+
+                    System.out.print("Nome: ");
+                    mecanicos[qtdMecanicos].nome = leia.nextLine();
+
+                    System.out.print("Especialidade: ");
+                    mecanicos[qtdMecanicos].especialidade = leia.nextLine();
+
+                    qtdMecanicos++;
+
+                    System.out.println("Mecânico cadastrado!");
+
                     break;
                 case 2:
                     break;
                 case 3:
+
+                    estoque[qtdEstoque] = new Estoque();
+
+                    System.out.print("Código: ");
+                    estoque[qtdEstoque].codigo = leia.nextInt();
+
+                    leia.nextLine();
+
+                    System.out.print("Descrição: ");
+                    estoque[qtdEstoque].descricao = leia.nextLine();
+
+                    System.out.print("Quantidade: ");
+                    estoque[qtdEstoque].quantidade = leia.nextInt();
+
+                    System.out.print("Preço: ");
+                    estoque[qtdEstoque].preco = leia.nextDouble();
+
+                    qtdEstoque++;
+
+                    System.out.println("Peça cadastrada!");
+
                     break;
                 case 4:
                     break;
